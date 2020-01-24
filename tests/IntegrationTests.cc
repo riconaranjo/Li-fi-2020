3370b83bee83d7031b407c5eb93e3adc68a1c040
Federico <federico.naranjo@gmail.com>
2020-01-24 17:02:23 -0500

    starting initial integration tests implementation

---
+++ tests/IntegrationTests.cc
@@ -0,0 +1,103 @@
+// include statements //
+
+#include <iostream>       // std::cout
+#include "IntegrationTests.h"
+
+// constructor
+IntegrationTests::IntegrationTests() {
+
+}
+
+// destructor
+IntegrationTests::~IntegrationTests() {
+
+}
+
+// tests //
+
+void IntegrationTests::runAll() {
+    CharacterTransmissionTest();
+    SpeedTransmissionTest();
+    ConnectDevicesTest();
+    DisconnectDevicesTest();
+    TransmittingCharactersTest();
+    TransmittingDataTest();
+}
+
+// UC-1
+bool IntegrationTests::CharacterTransmissionTest() {
+    bool result = false;
+
+    std::cout << "IntegrationTests::CharacterTransmissionTest() not implemented\n";
+
+    if (result) {
+        std::cout << "- CharacterTransmissionTest (UC-1) failed\n"
+    } else {
+        std::cout << "+ CharacterTransmissionTest (UC-1) passed\n"
+    }
+}
+
+// UC-2
+bool IntegrationTests::SpeedTransmissionTest() {
+    bool result = false;
+
+    std::cout << "IntegrationTests::SpeedTransmissionTest() not implemented\n";
+
+    if (result) {
+        std::cout << "- SpeedTransmissionTest (UC-2) failed\n"
+    } else {
+        std::cout << "+ SpeedTransmissionTest (UC-2) passed\n"
+    }
+}
+
+// UC-3
+bool IntegrationTests::ConnectDevicesTest() {
+    bool result = false;
+
+    std::cout << "IntegrationTests::ConnectDevicesTest() not implemented\n";
+
+    if (result) {
+        std::cout << "- ConnectDevicesTest (UC-3) failed\n"
+    } else {
+        std::cout << "+ ConnectDevicesTest (UC-3) passed\n"
+    }
+}
+
+// UC-4
+bool IntegrationTests::DisconnectDevicesTest() {
+    bool result = false;
+
+    std::cout << "IntegrationTests::DisconnectDevicesTest() not implemented\n";
+
+    if (result) {
+        std::cout << "- DisconnectDevicesTest (UC-4) failed\n"
+    } else {
+        std::cout << "+ DisconnectDevicesTest (UC-4) passed\n"
+    }
+}
+
+// UC-5
+bool IntegrationTests::TransmittingCharactersTest() {
+    bool result = false;
+
+    std::cout << "IntegrationTests::TransmittingCharactersTest() not implemented\n";
+
+    if (result) {
+        std::cout << "- TransmittingCharactersTest (UC-5) failed\n"
+    } else {
+        std::cout << "+ TransmittingCharactersTest (UC-5) passed\n"
+    }
+}
+
+// UC-6
+bool IntegrationTests::TransmittingDataTest() {
+    bool result = false;
+
+    std::cout << "IntegrationTests::TransmittingDataTest() not implemented\n";
+
+    if (result) {
+        std::cout << "- TransmittingDataTest (UC-6) failed\n"
+    } else {
+        std::cout << "+ TransmittingDataTest (UC-6) passed\n"
+    }
+}
---
+++ tests/IntegrationTests.h
@@ -0,0 +1,64 @@
+#ifndef INTEGRATIONTESTS_H
+#define INTEGRATIONTESTS_H
+
+#include "CharacterTransmissionControl.cpp"
+#include "CharacterTransmissionControl.h"
+#include "ConnectDevicesControl.cpp"
+#include "ConnectDevicesControl.h"
+#include "Connection.cpp"
+#include "Connection.h"
+#include "ControlMessage.cpp"
+#include "ControlMessage.h"
+#include "Data.cpp"
+#include "Data.h"
+#include "DataFile.cpp"
+#include "DataFile.h"
+#include "DataMessage.cpp"
+#include "DataMessage.h"
+#include "DisconnectDevicesControl.cpp"
+#include "DisconnectDevicesControl.h"
+#include "Display.cpp"
+#include "Display.h"
+#include "ExternalDisplay.cpp"
+#include "ExternalDisplay.h"
+#include "FPGA.cpp"
+#include "FPGA.h"
+#include "Keyboard.cpp"
+#include "Keyboard.h"
+#include "Message.cpp"
+#include "Message.h"
+#include "Modem.cpp"
+#include "Modem.h"
+#include "SevenSegmentDisplay.cpp"
+#include "SevenSegmentDisplay.h"
+#include "SpeedTransmissionControl.cpp"
+#include "SpeedTransmissionControl.h"
+#include "TransmissionSpeed.cpp"
+#include "TransmissionSpeed.h"
+#include "TransmittingCharacterControl.cpp"
+#include "TransmittingCharacterControl.h"
+#include "TransmittingDataControl.cpp"
+#include "TransmittingDataControl.h"
+
+class IntegrationTests {
+public:
+    // constructor
+    IntegrationTests();
+    // destructor
+    ~IntegrationTests();
+
+    // tests //
+
+    void runAll();
+    void setUpTest();
+
+    bool CharacterTransmissionTest();   // UC-1
+    bool SpeedTransmissionTest();       // UC-2
+    bool ConnectDevicesTest();          // UC-3
+    bool DisconnectDevicesTest();       // UC-4
+    bool TransmittingCharactersTest();  // UC-5
+    bool TransmittingDataTest();        // UC-6
+};
+
+
+#endif // INTEGRATIONTESTS_H
